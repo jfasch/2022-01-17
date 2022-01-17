68a43028281c076326ce0d1087dd38dd51fd9e24
Joerg Faschingbauer <jf@faschingbauer.co.at>
2022-01-17 12:43:44 +0100

    first couple of files

---
+++ argv.py
@@ -0,0 +1,6 @@
+import sys
+
+print('argv:', sys.argv)
+print('len:', len(sys.argv))
+print('argv[0]:', sys.argv[0])
+print('argv[1]:', sys.argv[1])
---
+++ indentation.py
@@ -0,0 +1,11 @@
+#answer = 5
+#if answer == 42:
+#    print('the answer is', answer)
+#    print('das ist humbug')
+
+answer = 0
+while answer != 42:
+    print(answer, 'is still not the answer')
+    answer += 1
+    
+print('found the answer, it is', answer)
