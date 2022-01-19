8ad8d02ebb4d7d1dcc9781a71f9419610997f266
Joerg Faschingbauer <jf@faschingbauer.co.at>
2022-01-19 10:46:19 +0100

    uniq

--- uniq-set.py
+++ uniq-set.py
@@ -2,9 +2,11 @@ input_list = [2, 3, 1, 10, 3, 3, 1, 10, 5, 2]
 output_list = []
 
 
+have = set()    # speed optimization
 for num in input_list:
-    if not num in output_list:
+    if not num in have:
         output_list.append(num)
+        have.add(num)
 
 for num in output_list:
     print(num)
